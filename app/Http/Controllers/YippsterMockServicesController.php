e94527bf6fb1fb6829228e6b5163055e868fed65
Chi-Heng Huang <bryan.chi.h@mig.me>
2015-07-07 11:26:44 +0800

    progress check

--- app/Http/Controllers/YippsterMockServicesController.php
+++ app/Http/Controllers/YippsterMockServicesController.php
@@ -8,77 +8,13 @@ use Illuminate\Http\Request;
 class YippsterMockServicesController extends Controller {
 
 	/**
-	 * Display a listing of the resource.
+	 * Receive successful messages from "http://60.251.3.181:55555/yippster/notify"
 	 *
 	 * @return Response
 	 */
-	public function index()
+	public function success()
 	{
-		//
+		var_dump('Yippster mock services');
+		// var_dump(\Input::get());	
 	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
 }
