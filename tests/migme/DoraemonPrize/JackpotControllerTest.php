bf6991d6f1620983834109c6ab96738694103fb6
Chi-Heng Huang <bryan.chi.h@mig.me>
2015-07-08 20:03:09 +0800

    progress check on doraemon prize

--- tests/migme/DoraemonPrize/JackpotControllerTest.php
+++ tests/migme/DoraemonPrize/JackpotControllerTest.php
@@ -7,10 +7,12 @@ class JackpotControllerTest extends TestCase
 		parent::setUp();
 	}
 
+
 	public function tearDown()
 	{
 		parent::tearDown();
 	}
+
 
 	/**
 	 * Try request 'jackpot-it' url and test response.
@@ -20,8 +22,6 @@ class JackpotControllerTest extends TestCase
 		// var_dump('ddd');
 		// die;
 		$response = $this->call('GET', '/jackpot-it');	
-
-
 
 		// $this->assertEquals('email has been sent to the winner', $response->getContent());
 
