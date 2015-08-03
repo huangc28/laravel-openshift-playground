2a7aafb7b6042f30ad7b8ec8d62d73791aef2fef
Chi-Heng Huang <bryan.chi.h@mig.me>
2015-08-03 10:21:24 +0800

    user seeders add "created_at" and "updated_at" information

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 // composer require laracasts/testdummy
 use Laracasts\TestDummy\Factory as TestDummy;
+use Carbon\Carbon;
 
 class UserTableSeeder extends Seeder
 {
@@ -16,31 +17,39 @@ class UserTableSeeder extends Seeder
         // 'name' is the reference for $jiraTicket->assignee
         // @example
         DB::table('users')->insert([
-            'mig_id'   => 'achi.c',
-            'name'     => 'Achi Chen',
-            'email'    => 'achi.c@mig.me', 
-            'password' => '1234'
+            'mig_id'     => 'achi.c',
+            'name'       => 'Achi Chen',
+            'email'      => 'achi.c@mig.me', 
+            'password'   => '1234',
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString()
         ]);
 
         DB::table('users')->insert([
             'mig_id' => 'andre.l',  
             'name'   => 'Andre Lee',
             'email'  => 'andre.l@mig.me',
-            'password' => '1234'
+            'password'   => '1234',
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString()
         ]);
 
         DB::table('users')->insert([
             'mig_id' => 'bryan.ch.h',
             'name'   => 'bryan.ch.h', 
             'email'  => 'bryan.ch.h@mig.me',
-            'password' => '1234'
+            'password' => '1234',
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString()
         ]);
 
         DB::table('users')->insert([
             'mig_id' => 'damiano.t',
             'name'   => 'Damiano Tietto',
             'email'  => 'damiano.t@mig.me',
-            'password' => '1234'
+            'password' => '1234',
+            'created_at' => Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon::now()->toDateTimeString()
         ]);
     }
 }
